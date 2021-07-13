de5b78aa281089a058ee58378affb2a26c2350b6
rubjin <rubjin@gmail.com>
2021-07-13 15:13:17 +0900

    상담정보관리

--- resources/js/index.js
+++ resources/js/index.js
@@ -23,3 +23,83 @@ window.addEventListener("load", function () {
         }
     });
 });
+
+
+
+/* =====================================
+* side submenu accordion
+* =====================================
+*/
+$('.web-sidebar .has-submenu:first-child').addClass('active'); //첫번째 메뉴는 열림
+$(document).on('click', '.web-sidebar .has-submenu > .sidebar-1depth', function () {
+	$('.web-sidebar .sidebar-2depth.has-submenu:first-of-type').addClass('active'); //첫번째 메뉴는 열림
+	if ($(this).parent().hasClass('active')) {
+		$(this).next('.web-sidebar .sidebar-2depth').slideUp(200);
+		$(this).parent().removeClass('active');
+	} else {
+		$(this).next('.web-sidebar .sidebar-2depth').slideDown(200);
+		setTimeout(function () {
+			$('.web-sidebar .sidebar-2depth').css({ "overflow": "visible" });
+		}, 200);
+		$(this).parent().addClass('active');
+	}
+});
+
+$(document).on('click', '.web-sidebar .sidebar-2depth.has-submenu > a', function () {
+	if ($(this).parent('.sidebar-2depth').hasClass('active')) {
+		$(this).next('.web-sidebar .sidebar-3depth').slideUp(200);
+		$(this).parent('.sidebar-2depth').removeClass('active');
+	} else {
+		$(this).next('.web-sidebar .sidebar-3depth').slideDown(200);
+		setTimeout(function () {
+			$('.web-sidebar .sidebar-3depth').css({ "overflow": "visible" });
+		}, 200);
+		$(this).parent('.sidebar-2depth').addClass('active');
+	}
+
+});
+
+/* =====================================
+* 검색영역 기간선택 버튼
+* =====================================
+*/
+$(document).on('click', '.btn-period-box .btn', function() {
+	$('.btn-period-box .btn').removeClass('active');
+	$(this).addClass('active');
+});
+/* =====================================
+* datepicker
+* =====================================
+*/
+$(document).on('click', '.datepicker', function() {
+	$(this).datepicker({ 
+		dateFormat: 'yy/mm/dd' 
+	})
+	.datepicker( "show" );
+});
+
+/* =====================================
+* 헤더 시간 카운트다운
+* =====================================
+*/
+
+var time = 570; //기준 초
+var min = ""; //분
+var sec = "" //초
+
+var x= setInterval(function() {
+	min = parseInt(time/60);
+	sec = parseInt(time % 60);
+
+	if(min < 10) {
+		min = "0" + min;
+	}
+	if(sec < 10) {
+		sec = "0" + sec;
+	}
+	document.getElementById("timer").innerHTML = min + ":" + sec;
+	time--;
+	if(time < 0) {
+		clearInterval(x);
+	}
+}, 1000);
