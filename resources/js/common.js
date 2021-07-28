9b3694676c683917e090dd2b05472ec503841aa6
방휘랑 <bhlbest20@gmail.com>
2021-07-28 10:18:32 +0900

    [BP] 고객등록 - kendo Window 적용

--- resources/js/common.js
+++ resources/js/common.js
@@ -12,78 +12,77 @@ function includePath() {
 	var dataAttr = "data-include-path";
 	var include = $("[" + dataAttr + "]");
 	$.each(include, function (index, item) {
-	   var self = $(this);
-	   var path = self.attr(dataAttr);
-	   self.load(path, function() {
-		  $(this).before($(this).children())
-		  $(this).remove();
-		  kendo.bind($(".wrap"), {});
-	   });
+		var self = $(this);
+		var path = self.attr(dataAttr);
+		self.load(path, function () {
+			$(this).before($(this).children());
+			$(this).remove();
+			kendo.bind($(".wrap"), {});
+		});
 	});
- };
- 
- $(window).load(function () {
+}
+
+$(window).load(function () {
 	kendo.bind($(".wrap"), {});
 	includePath();
- });
+});
 
 /* =====================================
-* side submenu accordion
-* =====================================
-*/
-$('.sidebar-wrap .has-submenu:first-child').addClass('active'); //첫번째 메뉴는 열림
-$(document).on('click', '.sidebar-wrap .has-submenu > .sidebar-1depth', function () {
-	$('.sidebar-wrap .sidebar-2depth.has-submenu:first-of-type').addClass('active'); //첫번째 메뉴는 열림
-	if ($(this).parent().hasClass('active')) {
-		$(this).next('.sidebar-wrap .sidebar-2depth').slideUp(200);
-		$(this).parent().removeClass('active');
+ * side submenu accordion
+ * =====================================
+ */
+$(".sidebar-wrap .has-submenu:first-child").addClass("active"); //첫번째 메뉴는 열림
+$(document).on("click", ".sidebar-wrap .has-submenu > .sidebar-1depth", function () {
+	$(".sidebar-wrap .sidebar-2depth.has-submenu:first-of-type").addClass("active"); //첫번째 메뉴는 열림
+	if ($(this).parent().hasClass("active")) {
+		$(this).next(".sidebar-wrap .sidebar-2depth").slideUp(200);
+		$(this).parent().removeClass("active");
 	} else {
-		$(this).next('.sidebar-wrap .sidebar-2depth').slideDown(200);
+		$(this).next(".sidebar-wrap .sidebar-2depth").slideDown(200);
 		setTimeout(function () {
-			$('.sidebar-wrap .sidebar-2depth').css({ "overflow": "visible" });
+			$(".sidebar-wrap .sidebar-2depth").css({ overflow: "visible" });
 		}, 200);
-		$(this).parent().addClass('active');
+		$(this).parent().addClass("active");
 	}
 });
 
-$(document).on('click', '.sidebar-wrap .sidebar-2depth.has-submenu > a', function () {
-	if ($(this).parent('.sidebar-2depth').hasClass('active')) {
-		$(this).next('.sidebar-wrap .sidebar-3depth').slideUp(200);
-		$(this).parent('.sidebar-2depth').removeClass('active');
+$(document).on("click", ".sidebar-wrap .sidebar-2depth.has-submenu > a", function () {
+	if ($(this).parent(".sidebar-2depth").hasClass("active")) {
+		$(this).next(".sidebar-wrap .sidebar-3depth").slideUp(200);
+		$(this).parent(".sidebar-2depth").removeClass("active");
 	} else {
-		$(this).next('.sidebar-wrap .sidebar-3depth').slideDown(200);
+		$(this).next(".sidebar-wrap .sidebar-3depth").slideDown(200);
 		setTimeout(function () {
-			$('.sidebar-wrap .sidebar-3depth').css({ "overflow": "visible" });
+			$(".sidebar-wrap .sidebar-3depth").css({ overflow: "visible" });
 		}, 200);
-		$(this).parent('.sidebar-2depth').addClass('active');
+		$(this).parent(".sidebar-2depth").addClass("active");
 	}
 });
 
 /* =====================================
-* 타이틀 영역 토글버튼 SA0101.html
-* =====================================
-*/
-$(document).on('click', '.funcToggle', function() {
-	$(this).toggleClass('active');
+ * 타이틀 영역 토글버튼 SA0101.html
+ * =====================================
+ */
+$(document).on("click", ".funcToggle", function () {
+	$(this).toggleClass("active");
 });
 
-
 /* =====================================
-* file upload
-* =====================================
-*/
-$(document).ready(function(){
-	$(document).on('change','.filebox .upload-hidden', function(){
-		if(window.FileReader){
+ * file upload
+ * =====================================
+ */
+$(document).ready(function () {
+	$(document).on("change", ".filebox .upload-hidden", function () {
+		if (window.FileReader) {
 			var filename = $(this)[0].files[0].name;
-			console.log('filename', filename);
+			console.log("filename", filename);
 		} else {
-			var filename = $(this).val().split('/').pop().split('\\').pop();
+			var filename = $(this).val().split("/").pop().split("\\").pop();
 		}
-		$(this).parent('.filebox').find('.h-input').val(filename);
+		$(this).parent(".filebox").find(".h-input").val(filename);
 	});
-}); 
+});
 /* =====================================
-* popup
-* =====================================
-*/
+ * popup
+ * =====================================
+ */
